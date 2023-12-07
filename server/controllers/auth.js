b822444630a9a4977f0f1f665834a91a5544bfb9
irfan-flutions <irfan@flutions.in>
2023-12-07 11:09:03 +0530

    my first app

---
+++ server/controllers/auth.js
@@ -0,0 +1,57 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../model/User.js";
+
+//REGISTER USER
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt); //password hashed
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000), //his property likely represents the number of times a user's profile has been viewed or visited. The value is a random integer between 0 and 9999, indicating the number of profile views.
+      impressions: Math.floor(Math.random() * 10000), //This property could represent the number of times a piece of content (e.g., an advertisement or a post) has been displayed or "impressed" on a user's screen. Like viewedProfile, it's also a random integer between 0 and 9999, indicating the number of times the content was displayed or viewed.
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
